93ca7f3c5d6752cb032430dfaf5c64ab4a552e6b
Goggags123 <wongsakorngag@gmail.com>
2021-05-03 20:10:40 +0700

    Example

---
+++ Controllers/RegisterController.cs
@@ -0,0 +1,34 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using golablint.Models;
+
+namespace golablint.Controllers
+{
+    public class RegisterController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [Route("~/api/register")]
+        [HttpPost]
+        // [ValidateAntiForgeryToken]
+        public IActionResult Index(string name, string surname, string email, string password) {
+            // Console.WriteLine(id);
+            if(!ModelState.IsValid) {
+                return BadRequest();;
+            }
+            User user = new User();
+            // user.name = name;
+            // user.surname = surname;
+            // user.email = email;
+            // user.password = password;
+            return View();
+        }
+    }
+}
