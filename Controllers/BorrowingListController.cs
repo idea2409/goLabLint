22424305f9619b1abc4e50a277b017141a3ae553
Goggags123 <wongsakorngag@gmail.com>
2021-05-24 05:35:41 +0700

    Add borrowing list route

--- Controllers/BorrowingListController.cs
+++ Controllers/BorrowingListController.cs
@@ -3,29 +3,32 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Logging;
-using golablint.Models;
 using golablint.Data;
+using golablint.Models;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 
-namespace golablint.Controllers
-{
-    public class BorrowingListController : Controller
-    {
+namespace golablint.Controllers {
+    public class BorrowingListController : Controller {
         private readonly ApplicationDbContext _db;
         public BorrowingListController(ApplicationDbContext db) {
             _db = db;
         }
 
-        [Route("~/borrowing-list/{id}")]
-        public IActionResult Index(string id)
-        {
+        [Route("~/borrowing-list",Name="borrowing-list")]
+        public IActionResult Index(string id) {
             Guid _id;
-            if(!Guid.TryParse(id,out _id)) {
+            if (!Guid.TryParse(id, out _id)) {
                 return BadRequest();
             }
-            ViewBag.borrowingList = _db.Borrowing.FromSqlRaw($"SELECT * FROM \"Borrowing\" WHERE userId = \'{id}\'");     
+            var borrowingList = from borrowing in _db.Set<Borrowing>()
+            join equipment in _db.Set<Equipment>()
+            on borrowing.equipment.id equals equipment.id
+            select new { borrowing, equipment };
+            ViewBag.borrowingList = JObject.Parse(JsonConvert.SerializeObject(Json(borrowingList))).GetValue("Value");
             return View();
         }
 
